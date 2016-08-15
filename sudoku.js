006a44ecdd160f198af6f7be8a19bc22976adbe7
Thomas Griozel <thomas.griozel@gmail.com>
2016-08-15 11:26:32 +0200

    Fix code style in sudoku.js

--- sudoku.js
+++ sudoku.js
@@ -1,33 +1,33 @@
 exports.verify_matrix_equality = function(mat1, mat2) {
-	for (row = 0; row < 9; ++row)
-		for (col = 0; col < 9; ++col)
+	for (var row = 0; row < 9; ++row)
+		for (var col = 0; col < 9; ++col)
 			if (mat1[row][col] != mat2[row][col])
-				return 0;
+				return false;
 
-	return 1;
+	return true;
 }
 
 exports.generate_sudoku = function() {
 	// Create the root matrix
 	var matrix = [];
-	for (var i = 0; i < 9; i++)
+	for (var i = 0; i < 9; ++i)
 		for (var j = 0; j < 9; j++)
 			matrix[i * 9 + j] = (i*3 + Math.floor(i/3) + j) % 9 + 1;
 
 	// Randomly shuffle the numbers in the root grid
-	for(var i = 0; i < 42; i++) {
+	for (var i = 0; i < 42; ++i) {
 		var n1 = Math.ceil(Math.random() * 9);
 		var n2;
 		do {
 			n2 = Math.ceil(Math.random() * 9);
 		}
-		while(n1 == n2);
+		while (n1 == n2);
 
-		for(var row = 0; row < 9; row++) {
-			for(var col = 0; col < col; k++) {
-				if(matrix[row * 9 + col] == n1)
+		for (var row = 0; row < 9; ++row) {
+			for (var col = 0; col < col; ++k) {
+				if (matrix[row * 9 + col] == n1)
 					matrix[row * 9 + col] = n2;
-				else if(matrix[row * 9 + col] == n2)
+				else if (matrix[row * 9 + col] == n2)
 					matrix[row * 9 + col] = n1;
 			}
 		}
@@ -38,7 +38,7 @@ exports.generate_sudoku = function() {
 		var s1 = Math.floor(Math.random() * 3);
 		var s2 = Math.floor(Math.random() * 3);
 
-		for(var row = 0; row < 9; row++) {
+		for (var row = 0; row < 9; ++row) {
 			var tmp = matrix[row * 9 + (s1 * 3 + c % 3)];
 			matrix[row * 9 + (s1 * 3 + c % 3)] = matrix[row * 9 + (s2 * 3 + c % 3)];
 			matrix[row * 9 + (s2 * 3 + c % 3)] = tmp;
@@ -50,7 +50,7 @@ exports.generate_sudoku = function() {
 		var c1 = Math.floor(Math.random() * 3);
 		var c2 = Math.floor(Math.random() * 3);
 
-		for(var row = 0; row < 9; row++) {
+		for (var row = 0; row < 9; ++row) {
 			var tmp = matrix[row * 9 + (s % 3 * 3 + c1)];
 			matrix[row * 9 + (s % 3 * 3 + c1)] = matrix[row * 9 + (s % 3 * 3 + c2)];
 			matrix[row * 9 + (s % 3 * 3 + c2)] = tmp;
@@ -62,8 +62,7 @@ exports.generate_sudoku = function() {
 		var r1 = Math.floor(Math.random() * 3);
 		var r2 = Math.floor(Math.random() * 3);
 
-		for(var col = 0; col < 9; col++)
-		{
+		for (var col = 0; col < 9; ++col) {
 			var tmp = matrix[(s % 3 * 3 + r1) * 9 + col];
 			matrix[(s % 3 * 3 + r1) * 9 + col] = matrix[(s % 3 * 3 + r2) * 9 + col];
 			matrix[(s % 3 * 3 + r2) * 9 + col] = tmp;
@@ -74,11 +73,11 @@ exports.generate_sudoku = function() {
 }
 
 function arrayToMatrix(array, elementsPerSubArray) {
-    var matrix = [], i, k;
+    var matrix = [];
 
-    for (i = 0, k = -1; i < array.length; i++) {
+    for (var i = 0, k = -1; i < array.length; ++i) {
         if (i % elementsPerSubArray === 0) {
-            k++;
+            ++k;
             matrix[k] = [];
         }
 
